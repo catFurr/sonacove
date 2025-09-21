cdeec1784197e7ba5341cf341e0f68f79d6b1914
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2025-09-21 19:45:53 +0300

    fix

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ import customSchema from './astro-env-schema';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue(), react()],
-  output: 'server',
+  output: 'static',
 
   env: {
     schema: customSchema,
@@ -48,7 +48,6 @@ export default defineConfig({
       minify: false, // Let cloudflare handle this.
     },
     ssr: {
-      noExternal: ['react-dom/server'],
       external: ['node:buffer', 'node:stream', 'node:events'],
     },
     resolve: {
